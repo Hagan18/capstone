de99e615828b5b98c2b82004e21f344914a537bc
Hagan18 <hagan18@gmail.com>
2017-10-31 20:16:31 -0500

    added stop and auto-restart after 3 seconds after initiating omxplayer to try and combat the rpis playing at different times

--- receiver.c
+++ receiver.c
@@ -23,9 +23,9 @@ int s;
 int z;
 char dgram[512];
 struct sockaddr_in adr;
-Display *display;
+//Display *display;
 unsigned int keycode;
-int playing=0; //flag used to determine if playing
+int start=0; //flag used to determine if start command found
 
 typedef struct{
 	char dgram[512];
@@ -37,6 +37,7 @@ typedef struct{
 static void bail(const char *on_what);
 void interperateMessage();
 void getMessage();
+void checkPlay();
 
 static void bail (const char *on_what) {
 	fputs(strerror(errno),stderr);
@@ -46,9 +47,27 @@ static void bail (const char *on_what) {
 	exit(1);
 }
 
+/*******************************************************
+ * Function used to check if play is initiated. If true,
+ * the program waits for oxplayer to load and then runs
+ * a system command which tells omxplayer to restart the 
+ * song. Used to combat the problem of unsynced clocks
+ ******************************************************/
+void checkPlay(void){
+	while(1){
+		if(start == 1){
+			sleep(3);
+			int status = system("xdotool key Left");
+			printf("Song restarted\n");
+			start=0;
+		}
+	}
+}
+
 void getMessage(void){
 	pthread_t t2;
 	while (1){
+		printf("waiting from port: %d\n",adr.sin_port);
 		z = recvfrom(s,	dgram,sizeof(dgram),0,(struct sockaddr *) &adr, &x);
 		printf("%d\n",y);
 		if (x < 0){
@@ -56,15 +75,18 @@ void getMessage(void){
 		}
 		if (z >= 0){
 			printf("connection made: y=%d\n",z);
-
 			//message received
 			pthread_create(&t2,NULL,(void*)interperateMessage,NULL);
+		}
+		else {
+			printf("No connection\n");
 		}
 
 	}
 }
 
 void interperateMessage(){
+	pthread_t t3;
 	if (z >= 0){
 		char *command = malloc(sizeof(char*)*50);
 		strcpy(command,"omxplayer -o local ");
@@ -77,9 +99,12 @@ void interperateMessage(){
 		strcpy(song,"blink.mp3");
 		strcat(command,song);
 		printf("%s\n",command);
-		if (strncmp(input,"kyle",1) == 0 && !playing) {
+		if (strncmp(input,"kyle",1) == 0 && !start) {
+			pthread_create(&t3,NULL,(void*)checkPlay,NULL);
+			start = 1;
 			int status = system(command);
-			playing = 1;
+			//pthread_create(&t3,NULL,(void*)checkPlay,NULL);
+			//start = 1;
 		}
 		else if(strncmp(input,"pause",1) == 0){
 			printf("pause statement\n");
@@ -91,6 +116,10 @@ void interperateMessage(){
 		else if(strncmp(input,"-",1) == 0){
 			int status = system("xdotool key minus");
 		}
+		else if(strncmp(input,"quit",1) == 0){
+			int status = system("xdotool key q");
+			printf("exiting omxplayer\n");
+		}
 		putchar('\n');
 		fflush(stdout);
 	}
@@ -100,7 +129,7 @@ int main(int argc, char **argv) {
 	int len_inet;
 	static int so_reuseaddr = TRUE;
 	static char *bc_addr = "127.255.255.255:9097";
-	pthread_t t1;
+	pthread_t t1,t3;
 
 	if (argc > 1){
 		//broadcast address
@@ -132,7 +161,9 @@ int main(int argc, char **argv) {
 		bail("bind(2)");
 
 	//start thread to receive data
+	//pthread_create(&t3,NULL,(void*)checkPlay,NULL);
 	pthread_create(&t1,NULL,(void*)getMessage,NULL);
+	//pthread_create(&t3,NULL,(void*)checkPlay,NULL);
 
 	while (1){
 		sleep(1000);
